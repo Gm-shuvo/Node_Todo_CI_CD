06bb3eb89d363bdadb2732723e4dbc244631c6de
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-05-01 08:44:43 +0600

    Dokerfile + dummy testcase Adding + delete 2 file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM node:12.2.0-alpine
-WORKDIR app
-COPY . .
+# Define the working directory inside the container
+WORKDIR /app
+# Copy the file from your host to your current location.
+COPY . /app
+# Run the command inside your image filesystem.
 RUN npm install
 RUN npm run test
+# Inform Docker that the container is listening on the specified port at runtime.
 EXPOSE 8000
+# Run the specified command within the container.
 CMD ["node","app.js"]
--- test.js
+++ test.js
@@ -1,42 +1,61 @@
 // Requiring module
-const assert = require('assert');
+const assert = require("assert");
 
 // We can group similar tests inside a describe block
 describe("Simple Calculations", () => {
-before(() => {
-	console.log( "This part executes once before all tests" );
-});
+  before(() => {
+    console.log("This part executes once before all tests");
+  });
 
-after(() => {
-	console.log( "This part executes once after all tests" );
-});
-	
-// We can add nested blocks for different tests
-describe( "Test1", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
-	});
-	
-	it("Is returning 5 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
-	});
+  after(() => {
+    console.log("This part executes once after all tests");
+  });
 
-	it("Is returning 6 when multiplying 2 * 3", () => {
-	assert.equal(2*3, 6);
-	});
-});
+  // We can add nested blocks for different tests
+  describe("Test1", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
 
-describe("Test2", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
-	});
-	
-	it("Is returning 4 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
-	});
+    it("Is returning 5 when adding 2 + 3", () => {
+      assert.equal(2 + 3, 5);
+    });
 
-	it("Is returning 8 when multiplying 2 * 4", () => {
-	assert.equal(2*4, 8);
-	});
-});
+    it("Is returning 6 when multiplying 2 * 3", () => {
+      assert.equal(2 * 3, 6);
+    });
+  });
+
+  describe("Test2", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
+
+    it("Is returning 4 when adding 2 + 3", () => {
+      assert.equal(2 + 3, 5);
+    });
+
+    it("Is returning 8 when multiplying 2 * 4", () => {
+      assert.equal(2 * 4, 8);
+    });
+    it("Is returning 10 when multiplying 2 * 5", () => {
+      assert.equal(2 * 5, 10);
+    });
+  });
+  describe("Test2", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
+
+    it("Is returning 4 when adding 2 + 3", () => {
+      assert.equal(2 + 3, 5);
+    });
+
+    it("Is returning 8 when multiplying 2 * 4", () => {
+      assert.equal(2 * 4, 8);
+    });
+    it("Is returning 10 when multiplying 2 * 5", () => {
+      assert.equal(2 * 5, 10);
+    });
+  });
 });
